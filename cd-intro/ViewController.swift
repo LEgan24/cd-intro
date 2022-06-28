fd565366890c69b1faeaf71f2fc0c7ab7727485f
Lynn <lynn_egan@horacemann.org>
2022-06-28 11:18:39 -0400

    Initial Commit

---
+++ cd-intro/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  cd-intro
+//
+//  Created by Lynn Egan on 6/28/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
